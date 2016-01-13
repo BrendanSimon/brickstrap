77c8f3e9d15e32dbe1a794ad3a1a2049704e2cff
Dushara Jayasinghe <dushara@successful.com.au>
2016-01-13 07:46:03 +0000

    (#11) Modem auto-connects during boot
    Created a systemd service to auto connect the modem to the network.
    
    This is implemented in a bash script that powers up the modem and, once
    available, brings up the network interface.
    
    It depends on the existing modem.py script.
    
    NOTE: The modem.py script differs from the one that was tested with.

---
+++ ind-efd/root/opt/sbin/modem-power-enable.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# The modem python script toggles the modem power
+# line
+function toggle_power {
+        /opt/sbin/modem.py 0.2
+}
+
+device=cdc-wdm0
+dev_file=/dev/${device}
+# If the modem is already ON, we need to power it down first to
+# start it in a known state
+if [ -e $dev_file ]
+then
+        toggle_power
+        while [ -e $dev_file ];
+        do
+                sleep 0.1
+        done
+fi
+toggle_power
+
+# Bring up the network connection once the network interface
+# is available
+until nmcli d | grep $device
+do
+        sleep 0.1
+done
+nmcli d connect cdc-wdm0
