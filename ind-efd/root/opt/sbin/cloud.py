2eca29ef05d3e894a52f7f5e987bd342dd1e3304
Brendan Simon <brendan.simon@etrix.com.au>
2016-01-25 16:40:36 +1100

    IND-EFD: Use queue to send csv data to cloud thread.
    Use a queue to send csv data to the cloud thread, where it can be post one or more records to the web portal in its own time.  This prevents the main sampling thread from blocking on the http post response (which can take seconds).

--- ind-efd/root/opt/sbin/cloud.py
+++ ind-efd/root/opt/sbin/cloud.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2
+
 ##############################################################################
 ##
 ##  Author:     Successful Endeavours Pty Ltd
@@ -17,17 +19,18 @@ import arrow
 import mmap
 import csv
 import requests
+import Queue as queue
 
 from cStringIO import StringIO
 
 ##============================================================================
 
 class Cloud_Thread(threading.Thread):
-  
-    def __init__(self, config, app_state):
+
+    def __init__(self, config, app_state, cloud_queue):
         threading.Thread.__init__(self)
-  
-        self.cloud = Cloud(config=config, app_state=app_state)
+
+        self.cloud = Cloud(config=config, app_state=app_state, cloud_queue=cloud_queue)
         self.running = True
 
     def run(self):
@@ -49,10 +52,11 @@ class Cloud(object):
 
     ##------------------------------------------------------------------------
 
-    def __init__(self, config, app_state):
+    def __init__(self, config, app_state, cloud_queue):
 
         self.config = config
         self.app_state = app_state
+        self.cloud_queue = cloud_queue
 
         self.init()
 
@@ -90,13 +94,13 @@ class Cloud(object):
 
     ##------------------------------------------------------------------------
 
-    def post_measurements_data(self, csv_data):
+    def post_measurements_data(self, data):
         '''Post measurements data to the cloud service.'''
 
-        data = self.measurements_log_csv_header + csv_data
+        #data = self.measurements_log_csv_header + csv_data
 
         r = requests.post(self.config.web_server_measurements_log, headers=self.web_server_headers, data=data)
-        if 0:
+        if 1:
             print("DEBUG: *******************************************")
             print("DEBUG: requests.post:")
             print("DEBUG: requests.headers: data = {}".format(self.web_server_headers))
@@ -114,115 +118,130 @@ class Cloud(object):
     def wait_and_process(self):
         '''wait for data and process it.'''
 
-#        r = select.select([self.dev_hand], [], [], self.select_timeout)
-#        #print("DEBUG: r = {!r}".format(r))
-#        if not r[0]:
-#            print("DEBUG: TIMEOUT: wait_and_process")
-#            return
-        m_log_path = self.app_state['measurements_log_path']
-        m_log_data = self.app_state['measurements_log_data']
+        #csv_data = []
+        csv_data = [self.measurements_log_csv_header]
 
-        #m_log_datetime = self.app_state['measurements_log_datetime']
-        m_log_datetime = self.app_state['capture_datetime_utc']
-        #capture_datetime_utc = self.app_state['capture_datetime_utc']
-        #capture_datetime_local = self.app_state['capture_datetime_local']
-
-
-
-        ## FIXME: more sophistated code below (buggy, not ready).
-        ## FIXME: just post latest measurements for now and return.
-        ## 
-        if m_log_data == self.last_measurements_log_data:
-            delay = 0.1
-            #delay = 3
-            #print("DEBUG: Cloud: sleeping for {} second/s.".format(delay))
-            #time.sleep(delay)
+        ## Block on receive queue for first time in the receive queue.
+        try:
+            item = self.cloud_queue.get(block=True, timeout=2)
+        except queue.Empty:
+            print("EXCEPTION: clould_queue timeout")
             return
-
-        #if m_log_data != self.last_measurements_log_data:
         else:
-            self.post_measurements_data(csv_data=m_log_data)
-            self.last_measurements_log_data = m_log_data
+            #print("DEBUG: appending first csv_data.")
+            csv_data.append(item)
+
+        ## Remove any remaining items in the queue without blocking
+        ## max limit of 10 items.
+        for i in range(10):
+            try:
+                item = self.cloud_queue.get(block=False)
+            except queue.Empty:
+                break
+            else:
+                #print("DEBUG: appending extra csv_data.")
+                csv_data.append(item)
+
+        ## concatenate csv row data to form a single string.
+        post_data = ''.join(csv_data)
+        #print("DEBUG: post_data={}".format(post_data))
+
+        self.post_measurements_data(data=post_data)
+
+        for i in range(len(csv_data)-1):
+            #print("DEBUG: cloud_queue.task_done()")
+            self.cloud_queue.task_done()      ## inform queue that item has been processed.
 
         return
 
-
-##FIXME: get the following going later !!
-
-        if m_log_data != self.last_measurements_log_data:
-            if 1:
-                print("DEBUG: Cloud: measurements_log_data changed:")
-                print("              last_data = {}".format(self.last_measurements_log_data))
-                print("              this_data = {}".format(m_log_data))
-            self.last_measurements_log_data = m_log_data
-
-            print("DEBUG: Cloud: Got data, processing ...")
-
-            if m_log_path != self.last_measurements_log_path:
-                ## Rollover of file, or no previous file.
-                if 1:
-                    print("DEBUG: Cloud: measurements_log_path changed:")
-                    print("              last_path = {}".format(self.last_measurements_log_path))
-                    print("              this_path = {}".format(m_log_path))
-
-                    ## Close memory mapped file.
-                    if self.measurements_log_mmap:
-                        self.measurements_log_mmap.close()
-
-                    ## Close file.
-                    if self.measurements_log_file:
-                        self.measurements_log_file.close()
-                
-                self.last_measurements_log_path = m_log_path
-
-            if not self.measurements_log_file:
-                mlf = self.measurements_log_file = open(m_log_path, 'r')
-                #with open(m_log_path, 'r') as mlf:
-                try:
-                    print("DEBUG: Cloud: mmap file: {}".format(m_log_path))
-                    mem = mmap.mmap(mlf.fileno(), length=0, access=mmap.ACCESS_READ, offset=0)
-                    self.measurements_log_mmap = mem
-                except:
-                    print("EXCEPTION: getting ADC Memory Map.")
-                    raise
-
-            if self.last_measurements_log_data:
-                print("DEBUG: Cloud: rfind: {}".format(self.last_measurements_log_data))
-                pos = mem.rfind(self.last_measurements_log_data)
-                print("DEBUG: Cloud: rfind: pos = {}".format(pos))
-                if pos >= 0:
-                    ## seek to pos, and post.
-                    pos += len(self.last_measurements_log_data)
-                    print("DEBUG: Cloud: increment pos = {}".format(pos))
-                    data = mem[pos:]
-                    print("DEBUG: Cloud: posting data ...")
-                    print("{}".format(data))
-                    self.post_measurements_data(csv_data=data)
-
-            ## log data for 
-            if self.last_measurements_log_datetime_utc.year > 2015:
-                pass
-            #csv_data = ''
-            #for s in self.dev_hand.readlines():
-            #self.post_measurements_data(csv_data)
-
-            #self.app_state['cloud_state'] = self.state
+        ##
+        ## FIXME: app_state was being used (abused) for inter-thread comms.
+        ## FIXME: should delete these old app_state entries as they are no longer used.
+        ## FIXME: this should be done during app initialisation.
+        ##
+        #m_log_path = self.app_state['measurements_log_path']
+        #m_log_data = self.app_state['measurements_log_data']
+        #m_log_datetime = self.app_state['measurements_log_datetime']
+        #m_log_datetime = self.app_state['capture_datetime_utc']
+        #capture_datetime_utc = self.app_state['capture_datetime_utc']
+        #capture_datetime_local = self.app_state['capture_datetime_local']
 
 ##============================================================================
 
 def main():
     """Main entry if running this module directly."""
 
-    cloud_thread = Cloud_Thread()
+    ##--------------------------------
+
+    class Config:
+        web_server_ping = 'http://portal.efdweb.com/api/Ping/0/'
+        web_server_measurements_log = 'http://portal.efdweb.com/api/AddEFDLog/0/'
+        measurements_log_field_names = [
+            'datetime_utc', 'datetime_local',
+            'max_volt_red', 'min_volt_red', 'max_time_offset_red', #'min_time_offset_red',
+            't2_red', 'w2_red',
+            'max_volt_wht', 'min_volt_wht', 'max_time_offset_wht', #'min_time_offset_wht',
+            't2_wht', 'w2_wht',
+            'max_volt_blu', 'min_volt_blu', 'max_time_offset_blu', #'min_time_offset_blu',
+            't2_blu', 'w2_blu',
+            'temperature', 'humidity', 'rain_intensity',
+            ]
+
+    config = Config()
+
+    ##--------------------------------
+
+    #measurements = {}
+    #measurements['datetime_utc']           = self.capture_datetime_utc.isoformat(sep=' ')
+    #measurements['datetime_local']         = self.capture_datetime_local.isoformat(sep=' ')
+    #measurements['max_volt_red']           = self.peak_max_red.voltage
+    #measurements['min_volt_red']           = self.peak_min_red.voltage
+    #measurements['max_time_offset_red']    = self.peak_max_red.time_offset
+    #measurements['min_time_offset_red']    = self.peak_min_red.time_offset
+    #measurements['t2_red']                 = self.tf_map_red.T2
+    #measurements['w2_red']                 = self.tf_map_red.F2
+    #measurements['max_volt_wht']           = self.peak_max_wht.voltage
+    #measurements['min_volt_wht']           = self.peak_min_wht.voltage
+    #measurements['max_time_offset_wht']    = self.peak_max_wht.time_offset
+    #measurements['min_time_offset_wht']    = self.peak_min_wht.time_offset
+    #measurements['t2_wht']                 = self.tf_map_wht.T2
+    #measurements['w2_wht']                 = self.tf_map_wht.F2
+    #measurements['max_volt_blu']           = self.peak_max_blu.voltage
+    #measurements['min_volt_blu']           = self.peak_min_blu.voltage
+    #measurements['max_time_offset_blu']    = self.peak_max_blu.time_offset
+    #measurements['min_time_offset_blu']    = self.peak_min_blu.time_offset
+    #measurements['t2_blu']                 = self.tf_map_blu.T2
+    #measurements['w2_blu']                 = self.tf_map_blu.F2
+    #measurements['temperature']            = self.ws_info.temperature
+    #measurements['humidity']               = self.ws_info.humidity
+    #measurements['rain_intensity']         = self.ws_info.rain_intensity
+
+    csv_str = '2016-01-12 15:04:12+00:00,2016-01-13 02:04:12+11:00,-0.00034332275390625,-0.0017547607421875,0.001766072,5.7265854614087719e-09,29205329.549096104,0.0016021728515625,-0.003662109375,0.00355964,5.7265854614087719e-09,31948299.751001682,-0.00034332275390625,-0.00171661376953125,0.001133052,5.7266116565935494e-09,29143762.475214832,20.2C,56.4P,0\n'
+
+    cloud_queue = queue.Queue()
+
+    cloud_thread = Cloud_Thread(config=config, app_state=None, cloud_queue=cloud_queue)
     cloud = cloud_thread.cloud
 
     #cloud_thread.init()
     cloud_thread.start()
-    delay = 2
+    delay = 3
+    data_count = [ 1, 5, 9, 100 ]
+    data_index = 0
     try:
         while True:
             print('Sleeping for {} seconds'.format(delay))
             time.sleep(delay)
+            count = data_count[data_index]
+            data_index += 1
+            if data_index >= len(data_count): data_index = 0
+            print("Queueing {} csv rows".format(count))
+            for i in range(count):
+                try:
+                    #cloud_queue.put(item=csv_str, block=False)
+                    cloud_queue.put(item=csv_str, block=True)
+                except queue.Full:
+                    print("EXCEPTION: queue is full.  i={}".format(i))
             print('----------------------------------------')
 
     except (KeyboardInterrupt, SystemExit):
@@ -230,9 +249,12 @@ def main():
         print("EXCEPTION: KeyboardInterrupt or SystemExit")
     finally:
         print("Nearly done ...")
+        print("joining queue ...")
+        cloud_queue.join()
+        print("queue joined.")
         cloud_thread.cleanup()
 
-    print "Done.\nExiting."
+    print("Done.  Exiting.")
 
 ##============================================================================
 
