107bf475e71d72233b45cf6edf9fe8a8080919fa
Brendan Simon <brendan.simon@etrix.com.au>
2016-02-02 13:17:48 +1100

    IND-EFD: Add command arguments to modem script.
    Add ability to power-on, power-off and power-cycle the modem by adding
    commands to the modem.py utility script.  The power-cycle command can take
    an optional delay parameter to specifiy the delay between turning the modem
    off and turning it back on (default is 0.5 seconds).

--- ind-efd/root/opt/sbin/modem.py
+++ ind-efd/root/opt/sbin/modem.py
@@ -12,33 +12,52 @@
 
 import sys
 import time
+import argh
 
 import ind
 
 ## could make this a runtime option.
 DEBUG = False
 
-if __name__ == "__main__":
-    try:
-        delay = float(sys.argv[1])
-    except:
-        raise Exception("must specify delay as first parameter")
+##============================================================================
 
-    with ind.get_device_handle() as dev_hand:
+def power_off():
+    """Power off the modem."""
+    print("Turning modem 'off'")
+    ind.modem_power_off(dev_hand=dev_hand)
 
-        while True:
-            ##
-            ## Pulse power key to turn it on.
-            ##
+##----------------------------------------------------------------------------
 
-            print("Turning modem 'off'")
-            ind.modem_power_off(dev_hand=dev_hand)
+def power_on():
+    """Power on the modem."""
+    print("Turning modem 'on'")
+    ind.modem_power_on(dev_hand=dev_hand)
 
-            print("Sleeping {} seconds ...".format(delay))
-            time.sleep(delay)
+##----------------------------------------------------------------------------
 
-            print("Turning modem 'on'")
-            ind.modem_power_on(dev_hand=dev_hand)
+#@argh.arg('delay', help='delay in seconds between turning off then on.')
 
-            break
+def power_cycle(delay=0.5):
+    """Power cycle the modem."""
+
+    power_off()
+    print("Sleeping {} seconds ...".format(delay))
+    time.sleep(delay)
+    power_on()
+
+##============================================================================
+
+if __name__ == '__main__':
+    ## assembling commands.
+    parser = argh.ArghParser()
+    commands = [ power_off, power_on, power_cycle ]
+    parser.add_commands(commands)
+
+    #with ind.get_device_handle() as dev_hand:
+    dev_hand = ind.get_device_handle()
+
+    ## dispatching commands.
+    parser.dispatch()
+
+##============================================================================
 
