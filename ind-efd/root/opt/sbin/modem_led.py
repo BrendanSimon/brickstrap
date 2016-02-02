d08f7d551ccee5ec4d9872f734681fdfda791495
Brendan Simon <brendan.simon@etrix.com.au>
2016-02-02 14:12:37 +1100

    IND-EFD: Add command arguments to modem led script.
    Use argh module to parse command line arguemnts to turn modem LED on or off.

--- ind-efd/root/opt/sbin/modem_led.py
+++ ind-efd/root/opt/sbin/modem_led.py
@@ -11,42 +11,66 @@
 import sys
 import time
 import ind
+import argh
 
 ## could make this a runtime option.
 DEBUG = False
 
-def main():
-    '''Main entry if running this module from command line.'''
+##============================================================================
 
-    app = sys.argv[0]
-
-    try:
-        status = sys.argv[1]
-    except:
-        status = None
-    
-    if not (status == '0' or status == '1'):
-        print("{app}: Invalid parameter: {param}".format(app=app, param=status))
-        return
+def modem_led_set(state):
+    """Set the modem LED."""
 
     led = ind.LED.Modem_OK
 
     on = 0
     off = 0
 
-    if status == '0':
+    if state:
+        ## Led on
+        on = led
+    else:
         ## Led off
         off = led
 
-    if status == '1':
-        ## Led on
-        on = led
-
-    dev_name = ind.dev_name
-    #with open(dev_name, 'rw') as dev_hand:
     with ind.get_device_handle() as dev_hand:
         ind.leds_modify(on=on, off=off, dev_hand=dev_hand)
 
+##============================================================================
+## Commands
+##============================================================================
+
+def off():
+    """Turn off the modem LED."""
+    modem_led_set(False)
+
+##----------------------------------------------------------------------------
+
+def on():
+    """Turn on the modem LED."""
+    modem_led_set(True)
+
+##============================================================================
+
+def main():
+    '''Main entry if running this module from command line.'''
+
+    ## assembling commands.
+    parser = argh.ArghParser()
+    commands = [ off, on ]
+    parser.add_commands(commands)
+
+    #with ind.get_device_handle() as dev_hand:
+    dev_hand = ind.get_device_handle()
+
+    ## dispatching commands.
+    parser.dispatch()
+
+
+##============================================================================
+
 if __name__ == "__main__":
     main()
+
+##============================================================================
 
