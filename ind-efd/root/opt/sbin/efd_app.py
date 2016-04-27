a95f5730806ea247e9198dafd1d5034ea009533c
Brendan Simon <brendan@successful.com.au>
2016-04-27 10:57:30 +1000

    IND-EFD: Refactor GPS Poller.
    Refactor GPS_Poller class from efd_app.py to its own module.

--- ind-efd/root/opt/sbin/efd_app.py
+++ ind-efd/root/opt/sbin/efd_app.py
@@ -35,6 +35,7 @@ from persistent_dict import PersistentDict
 
 from weather import Weather_Station_Thread
 from cloud import Cloud_Thread
+from efd_gps import GPS_Poller_Thread
 
 from efd_measurements import Measurements_Log
 
@@ -47,28 +48,6 @@ import tf_mapping
 #tf_mapping.DEBUG = True
 
 import ind
-
-##============================================================================
-
-## FIXME: should probably be in a separate module.
-
-class GPS_Poller_Thread(threading.Thread):
-
-    def __init__(self):
-        threading.Thread.__init__(self)
-
-        self.gpsd = gps.gps(mode=gps.WATCH_ENABLE) #starting the stream of info
-        self.running = True #setting the thread running to true
-
-    def run(self):
-        while self.running:
-            self.gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
-
-    def cleanup(self):
-        print('INFO: GPS_Poller_Thread: Cleaning up ...')
-        self.running = False
-        self.join() # wait for the thread to finish what it's doing
-        print('INFO: GPS_Poller_Thread: Done.')
 
 ##============================================================================
 
