63bef1bcaf4dc85d4e468d741b37748c8781a7da
Brendan Simon <brendan@successful.com.au>
2016-04-27 10:54:04 +1000

    IND-EFD: Refactor Measurement Log.
    Refactor Measurement_Log class from efd_app.py to its own module.

--- ind-efd/root/opt/sbin/efd_app.py
+++ ind-efd/root/opt/sbin/efd_app.py
@@ -36,11 +36,13 @@ from persistent_dict import PersistentDict
 from weather import Weather_Station_Thread
 from cloud import Cloud_Thread
 
+from efd_measurements import Measurements_Log
+
 #import peak_detect
 
 from tf_mapping import TF_Map
 from tf_mapping import tf_map_calculate
-## Disable debbuing in tf_mapping module.
+## Disable debugging in tf_mapping module.
 import tf_mapping
 #tf_mapping.DEBUG = True
 
@@ -331,85 +333,6 @@ class Config(object):
 
 ##============================================================================
 
-##FIXME: the entire logging of measurements should happen in another thread.
-##FIXME: including saving the csv file, posting to the web and sending sms alerts.
-class Measurements_Log(object):
-    '''Manage logging measurement data to log files.'''
-    '''Uses csv module and csv.DictWriter object.'''
-    '''Rotate log files each day, based on 'datetime_utc' field.'''
-
-    ## FIXME: not sure if putting the measurements_log_field_names in the config object is the right thing.
-
-    def __init__(self, app_state, cloud_queue, url, field_names=Config.measurements_log_field_names):
-        self.app_state = app_state
-        self.cloud_queue = cloud_queue
-        self.url = url
-        self.field_names = field_names
-        self.csv_file = None
-        self.path = os.path.join(os.sep, 'mnt', 'data', 'log', 'measurements')
-        self.filename = ''
-        self.filename_prefix = 'measurements-'
-        self.filename_extension = '.csv'
-        self.day_saved = 0
-
-        ## initialise csv header string.
-        hdr_sio = StringIO()
-        writer = csv.DictWriter(hdr_sio, fieldnames=self.field_names, extrasaction='ignore')
-        writer.writeheader()
-        self.csv_header = hdr_sio.getvalue()
-        hdr_sio.close()
-
-    def init(self):
-        '''Runtime intialisation method.'''
-        ## Create directory to store measurements log if it doesn't exists.
-        if not os.path.exists(self.path):
-            os.makedirs(self.path)
-
-    def write(self, measurements, datetime):
-        dt = datetime
-        if not self.filename or (dt.day != self.day_saved):
-            dt_str = dt.floor('day').format('YYYYMMDDTHHmmssZ')
-            #dt_str = dt.format('YYYYMMDDTZ')
-            #dt_str = dt.format('YYYYMMDDZ')
-            filename = '{prefix}{dtstr}{extension}'.format(prefix=self.filename_prefix, dtstr=dt_str, extension=self.filename_extension)
-            self.filename = filename
-
-
-        self.day_saved = dt.day
-
-        ## format csv output to a string (not a file) so the same output
-        ## can be efficiently saved to a file and post to web server.
-        row_sio= StringIO()
-        writer = csv.DictWriter(row_sio, fieldnames=self.field_names, extrasaction='ignore')
-        writer.writerow(measurements)
-        self.csv_data = row_sio.getvalue()
-        row_sio.close()
-
-        ##
-        ## Write measurements to log file; and header if it's a new file.
-        ##
-        path_filename = os.path.join(self.path, self.filename)
-        new_file = not os.path.exists(path_filename)
-        with open(path_filename, 'a') as csvfile:
-            if new_file:
-                ## write header to new file.
-                csvfile.write(self.csv_header)
-            ## write measurements to file.
-            csvfile.write(self.csv_data)
-
-        ##
-        ## use queue to send the csv row to the cloud thread.
-        ## NOTE: could send the measurement dict to the cloud thread and let it:
-        ## generate the csv for saving to file and posting to web, and send sms.
-        ##
-        try:
-            self.cloud_queue.put(item=self.csv_data, block=False)
-        except queue.Full:
-            print("EXCEPTION: could not queue measurement data to cloud thread. qsize={}".format(self.cloud_queue.qsize()))
-            sys.stdout.flush()
-
-##============================================================================
-
 class EFD_App(object):
     '''The IND Early Fault Detection application class.'''
 
@@ -474,7 +397,7 @@ class EFD_App(object):
         self.cloud = self.cloud_thread.cloud
 
         self.measurements = {}
-        self.measurements_log = Measurements_Log(app_state=self.app_state, cloud_queue=self.cloud_queue, url=self.config.web_server_measurements_log)
+        self.measurements_log = Measurements_Log(cloud_queue=self.cloud_queue, url=self.config.web_server_measurements_log, field_names=self.config.measurements_log_field_names)
 
         self.last_pd_event_report_datetime_utc = arrow.Arrow(1,1,1)
 
