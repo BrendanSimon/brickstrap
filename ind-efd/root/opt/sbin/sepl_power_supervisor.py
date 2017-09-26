c384ced15099f9efcb1523a1e9a550f5236d2b18
Brendan Simon <brendan@successful.com.au>
2017-09-26 22:38:15 +1000

    [IND2,BB#103] Graceful shutdown when shutdown button pressed.
    * `systemd` service to monitor the state of the `SHUTDOWN_REQUEST` signal
      and execute the `/sbin/poweroff` command when detected.
    
    * `systemd` service to flash LEDs and deassert the `nOS_RUNNING` signal
      late(ish) during the system shutdown procedure.

---
+++ ind-efd/root/opt/sbin/sepl_power_supervisor.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python2
+
+from time import sleep
+import subprocess
+
+import ind
+
+##============================================================================
+
+shutdown_cmd = "/sbin/poweroff"
+
+delay = 1
+
+##============================================================================
+
+def led_off(dev_hand=None):
+    ind.battery_led_off(dev_hand=dev_hand)
+    ind.power_led_off(dev_hand=dev_hand)
+
+##============================================================================
+
+def led_on(dev_hand=None):
+    ind.battery_led_on(dev_hand=dev_hand)
+    ind.power_led_on(dev_hand=dev_hand)
+
+##============================================================================
+
+def shutdown():
+    print("Calling command: {}".format(shutdown_cmd))
+    subprocess.call(shutdown_cmd)
+    
+##============================================================================
+
+def main():
+
+    with open("/dev/IND") as dev_hand:
+        ## Indicate we are up and running.
+        ind.power_os_running_on(dev_hand=dev_hand)
+        led_on(dev_hand=dev_hand)
+
+        while True:
+            shutdown_req = ind.power_shutdown_requested(dev_hand=dev_hand)
+            if shutdown_req:
+                shutdown()
+
+            sleep(delay)
+            print(".")
+
+##============================================================================
+
+if __name__ == "__main__":
+    main()
+
