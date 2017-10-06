91049ba25dd9be96ef223e59c3f1f0df8f23fa12
Brendan Simon <brendan@successful.com.au>
2017-10-06 15:50:08 +1100

    [test_serial.py] Add simple serial loopback test util.

---
+++ ind-efd/root/opt/sbin/test/test_serial.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2
+
+import argh
+import sys
+from serial import Serial
+
+##============================================================================
+
+#def serial_loopback(port=sys.argv[1], baudrate=115200, timeout=1):
+def serial_loopback(port='', baudrate=115200, timeout=1):
+
+    test_str = 'abc123'
+
+    with Serial(port, baudrate=baudrate, timeout=timeout) as ser:
+        ser.write(test_str)
+        in_str = ser.read(len(test_str))
+        if in_str != test_str:
+            raise Exception("Loopback error: sent: {!r}, received: {!r}".format((
+test_str, in_str))
+
+##============================================================================
+
+def main():
+    """Main entry if running this module from command line."""
+    argh.dispatch_command(serial_loopback)
+
+##============================================================================
+
+if __name__ == "__main__":
+    main()
+
