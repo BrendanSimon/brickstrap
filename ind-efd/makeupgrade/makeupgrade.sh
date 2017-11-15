7c4b2bcecd11e38ac0693b72195e5a4dea98ea14
Brendan Simon <brendan@successful.com.au>
2017-11-15 11:17:19 +1100

    [IND,makeupgrade] Auto-generate upgrade directory and exec `makeself`.

--- ind-efd/makeupgrade/makeupgrade.sh
+++ ind-efd/makeupgrade/makeupgrade.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+## Exit on any error.
+set -o errexit
+
 ##
 ## Make a runnable patch file from contents of a directory.
 ## Uses the `makeself` utility.
@@ -17,9 +20,27 @@ upgrade_desc="IND EFD Upgrade to '${ver}'"
 
 upgrade_run="./run.sh"
 
+upgrade_archive="ind-efd-${ver}.tar.gz"
+
+prog_rel=$0
+prog=$(basename ${prog_rel})
+
+source_dir=$(dirname ${prog_rel})
+
+##===========================================================================
+
 echo "Making '${upgrade_desc}'"
 
+## Make upgrade directory, copy in run script and other files.
+rm --force --recursive "${upgrade_dir}"
+mkdir --parents "${upgrade_dir}"
+rsync "${source_dir}/${upgrade_run}" "${upgrade_dir}/"
+rsync "${upgrade_archive}" "${upgrade_dir}/"
+
+## Makeself
 makeself ${upgrade_dir} ${upgrade_out} "${upgrade_desc}" ${upgrade_run}
 
 echo "Finished making '${upgrade_desc}'"
+
+##===========================================================================
 
