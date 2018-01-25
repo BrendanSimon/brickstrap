0140b72bd604b9dbd9eb5dbf0a74697c2b704da1
Brendan Simon <brendan@successful.com.au>
2018-01-25 15:37:52 +1100

    [IND] Bump version to `0.11.0`

--- ind-efd/root/opt/sbin/efd_config.py
+++ ind-efd/root/opt/sbin/efd_config.py
@@ -35,7 +35,7 @@ class Config(object):
     #! Default values.  Can be overridden by settings file or command line.
     #!
 
-    version_str = '0.10.3-dev'
+    version_str = '0.11.0'
 
     serial_number = '0'
 
