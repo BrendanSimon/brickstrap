7f1c6a6046b549fa69870269bdc478bcb3fd6025
Brendan Simon (SEPL) <brendan@successful.com.au>
2018-03-16 00:45:04 +1100

    [IND,efd_app] Fix typo/bug in peak detection sanity check.
    * `timeoffset` => `time_offset`

--- ind-efd/root/opt/sbin/efd_app.py
+++ ind-efd/root/opt/sbin/efd_app.py
@@ -1411,7 +1411,7 @@ class EFD_App(object):
         #!
         test_max = abs(self.peak_normal_max_red.voltage) > abs(self.peak_normal_min_red.voltage)
         test_min = abs(self.peak_normal_max_red.voltage) < abs(self.peak_normal_min_red.voltage)
-        test_max |= not (test_max or test_min) and self.peak_normal_max_red.time_offset < self.peak_normal_min_red.timeoffset
+        test_max |= not (test_max or test_min) and self.peak_normal_max_red.time_offset < self.peak_normal_min_red.time_offset
 
         if test_max:
             v2, sq = (self.peak_normal_max_red.voltage ** 2), self.peak_squared_max_red.voltage
@@ -1420,7 +1420,7 @@ class EFD_App(object):
                 errors += 1
             t1, t2 = self.peak_normal_max_red.time_offset, self.peak_squared_max_red.time_offset
             if t2 != t1:
-                logging.error("peak_normal_max_red.timeoffset={} does not equal peak_squared_max_red.timeoffset={}".format(t2, t1))
+                logging.error("peak_normal_max_red.time_offset={} does not equal peak_squared_max_red.time_offset={}".format(t2, t1))
                 errors += 1
         else:
             v2, sq = (self.peak_normal_min_red.voltage ** 2), self.peak_squared_max_red.voltage
@@ -1429,7 +1429,7 @@ class EFD_App(object):
                 errors += 1
             t1, t2 = self.peak_normal_min_red.time_offset, self.peak_squared_max_red.time_offset
             if t2 != t1:
-                logging.error("peak_normal_min_red.timeoffset={} does not equal peak_squared_max_red.timeoffset={}".format(t2, t1))
+                logging.error("peak_normal_min_red.time_offset={} does not equal peak_squared_max_red.time_offset={}".format(t2, t1))
                 errors += 1
 
         #!
@@ -1437,7 +1437,7 @@ class EFD_App(object):
         #!
         test_max = abs(self.peak_normal_max_wht.voltage) > abs(self.peak_normal_min_wht.voltage)
         test_min = abs(self.peak_normal_max_wht.voltage) < abs(self.peak_normal_min_wht.voltage)
-        test_max |= not (test_max or test_min) and self.peak_normal_max_wht.time_offset < self.peak_normal_min_wht.timeoffset
+        test_max |= not (test_max or test_min) and self.peak_normal_max_wht.time_offset < self.peak_normal_min_wht.time_offset
 
         if test_max:
             v2, sq = (self.peak_normal_max_wht.voltage ** 2), self.peak_squared_max_wht.voltage
@@ -1446,7 +1446,7 @@ class EFD_App(object):
                 errors += 1
             t1, t2 = self.peak_normal_max_wht.time_offset, self.peak_squared_max_wht.time_offset
             if t2 != t1:
-                logging.error("peak_normal_max_wht.timeoffset={} does not equal peak_squared_max_wht.timeoffset={}".format(t2, t1))
+                logging.error("peak_normal_max_wht.time_offset={} does not equal peak_squared_max_wht.time_offset={}".format(t2, t1))
                 errors += 1
         else:
             v2, sq = (self.peak_normal_min_wht.voltage ** 2), self.peak_squared_max_wht.voltage
@@ -1455,7 +1455,7 @@ class EFD_App(object):
                 errors += 1
             t1, t2 = self.peak_normal_min_wht.time_offset, self.peak_squared_max_wht.time_offset
             if t2 != t1:
-                logging.error("peak_normal_min_wht.timeoffset={} does not equal peak_squared_max_wht.timeoffset={}".format(t2, t1))
+                logging.error("peak_normal_min_wht.time_offset={} does not equal peak_squared_max_wht.time_offset={}".format(t2, t1))
                 errors += 1
 
         #!
@@ -1463,7 +1463,7 @@ class EFD_App(object):
         #!
         test_max = abs(self.peak_normal_max_blu.voltage) > abs(self.peak_normal_min_blu.voltage)
         test_min = abs(self.peak_normal_max_blu.voltage) < abs(self.peak_normal_min_blu.voltage)
-        test_max |= not (test_max or test_min) and self.peak_normal_max_blu.time_offset < self.peak_normal_min_blu.timeoffset
+        test_max |= not (test_max or test_min) and self.peak_normal_max_blu.time_offset < self.peak_normal_min_blu.time_offset
 
         if test_max:
             v2, sq = (self.peak_normal_max_blu.voltage ** 2), self.peak_squared_max_blu.voltage
@@ -1472,7 +1472,7 @@ class EFD_App(object):
                 errors += 1
             t1, t2 = self.peak_normal_max_blu.time_offset, self.peak_squared_max_blu.time_offset
             if t2 != t1:
-                logging.error("peak_normal_max_blu.timeoffset={} does not equal peak_squared_max_blu.timeoffset={}".format(t2, t1))
+                logging.error("peak_normal_max_blu.time_offset={} does not equal peak_squared_max_blu.time_offset={}".format(t2, t1))
                 errors += 1
         else:
             v2, sq = (self.peak_normal_min_blu.voltage ** 2), self.peak_squared_max_blu.voltage
@@ -1481,7 +1481,7 @@ class EFD_App(object):
                 errors += 1
             t1, t2 = self.peak_normal_min_blu.time_offset, self.peak_squared_max_blu.time_offset
             if t2 != t1:
-                logging.error("peak_normal_min_blu.timeoffset={} does not equal peak_squared_max_blu.timeoffset={}".format(t2, t1))
+                logging.error("peak_normal_min_blu.time_offset={} does not equal peak_squared_max_blu.time_offset={}".format(t2, t1))
                 errors += 1
 
         return errors
