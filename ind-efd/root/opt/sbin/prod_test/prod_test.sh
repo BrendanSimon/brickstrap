05b83309c1db0e234035cf69c6b404980671891b
Brendan Simon (SEPL) <brendan@successful.com.au>
2018-10-18 10:55:01 +1100

    [IND][prod_test.sh] Remove stdout/stderr redirection
    * Remove stdout/stderr redirection as this now done by the
    `prod_test.py` script.
    
    * Fixes issues with passing ctrl-c interrupt to python app for proper
    app cleanup.
    
    Also:
    
    * trap exit so that cleanup routine can run (even for ctrl-c interrupt)

--- ind-efd/root/opt/sbin/prod_test/prod_test.sh
+++ ind-efd/root/opt/sbin/prod_test/prod_test.sh
@@ -2,24 +2,68 @@
 
 set -e
 
-PROD_TEST_DIR="/opt/sbin/prod_test"
+BOOT_DIR="/boot/flash"
 
-PROD_TEST="${PROD_TEST_DIR}/prod_test.py"
+PROG_DIR="${PROG_DIR:-/opt/sbin/prod_test}"
 
-LOG_DIR="/boot/flash"
-LOG_FILE="${LOG_DIR}/prodtest.txt"
-LOG_FILE_TMP="/tmp/prod_tmp.txt"
+PROG="${PROG:-${PROG_DIR}/prod_test.py}"
 
-sudo mount -o remount,rw ${LOG_DIR}
+LOG_DIR="${LOG_DIR:-${BOOT_DIR}}"
 
-#cd ${PROD_TEST_DIR}
-#pwd
-#echo "Executing ${PROD_TEST}"
-sudo ${PROD_TEST} "$@" 2>&1 | tee ${LOG_FILE_TMP}
-sudo bash -c "cat ${LOG_FILE_TMP} >> ${LOG_FILE}"
-rm ${LOG_FILE_TMP}
+LOG_FILE="${LOG_FILE:-${LOG_DIR}/prodtest.txt}"
 
-sync
-sudo mount -o remount,ro ${LOG_DIR}
-sync
 
+
+#! is log file in the read-only boot filesystem?
+#! use base (( )) arithmentic operator to evaluate.
+#! e.g. if (( log_file_in_boot )) ...
+log_file_in_boot=$( [[ "${LOG_FILE}" == "${BOOT_DIR}"* ]] && echo 1 || echo 0 )
+
+
+
+#!
+#! cleanup function
+#!
+function cleanup
+{
+    #echo "CLEANUP"
+
+    #!
+    #! Remount the boot filesystem with read-only permission
+    #!
+    sync
+    if (( log_file_in_boot )) ; then
+        echo "Mount boot filesystem read-only"
+        sudo -- mount -o remount,ro "${BOOT_DIR}"
+    fi
+    sync
+}
+
+#! call cleanup function on exit (even for ctrl-c interrupt)
+trap cleanup EXIT
+
+
+
+#!
+#! Remount the boot filesystem with read-write permission
+#!
+if (( log_file_in_boot )) ; then
+    echo "Mount boot filesystem read-write"
+    sudo -- mount -o remount,rw "${BOOT_DIR}"
+fi
+
+
+
+#!
+#! Execute prod_test.py script
+#!
+
+#cd ${PROG_DIR}
+#echo "Executing ${PROG}"
+
+sudo -- "${PROG}" -o "${LOG_FILE}" "$@"
+
+
+
+#! Exit
+#! cleanup function will be called now (due to trap exit)
