b9dd889f291b354da5ca441692719bc72869e78b
Brendan Simon (SEPL) <brendan@successful.com.au>
2018-10-18 11:45:50 +1100

    [IND][adc_offset.sh] Better handling of file redirection to log file
    * Allows colours to be diplayed
    
    * Fixes issues with passing ctrl-c interrupt to python app for proper
    app cleanup.
    
    * trap exit so that cleanup routine can run (even for ctrl-c interrupt)

--- ind-efd/root/opt/sbin/prod_test/adc_offset.sh
+++ ind-efd/root/opt/sbin/prod_test/adc_offset.sh
@@ -2,24 +2,81 @@
 
 set -e
 
-PROG_DIR="/opt/sbin/prod_test"
+BOOT_DIR="/boot/flash"
 
-PROG="${PROG_DIR}/adc_offset.py"
+PROG_DIR="${PROG_DIR:-/opt/sbin/prod_test}"
 
-LOG_DIR="/boot/flash"
-LOG_FILE="${LOG_DIR}/adc_off.txt"
-LOG_FILE_TMP="/tmp/adc_tmp.txt"
+PROG="${PROG:-${PROG_DIR}/adc_offset.py}"
 
-sudo mount -o remount,rw ${LOG_DIR}
+LOG_DIR="${LOG_DIR:-${BOOT_DIR}}"
+
+LOG_FILE="${LOG_FILE:-${LOG_DIR}/adc_off.txt}"
+
+LOG_FILE_TMP="${LOG_FILE_TMP:-/tmp/adc_tmp.txt}"
+
+
+
+#! is log file in the read-only boot filesystem?
+#! use base (( )) arithmentic operator to evaluate.
+#! e.g. if (( log_file_in_boot )) ...
+log_file_in_boot=$( [[ "${LOG_FILE}" == "${BOOT_DIR}"* ]] && echo 1 || echo 0 )
+
+
+
+#!
+#! cleanup function
+#!
+function cleanup
+{
+    #echo "CLEANUP"
+
+    rm -f "${LOG_FILE_TMP}"
+
+    #!
+    #! Remount the boot filesystem with read-only permission
+    #!
+    sync
+    if (( log_file_in_boot )) ; then
+        echo "Mount boot filesystem read-only"
+        sudo -- mount -o remount,ro "${BOOT_DIR}"
+    fi
+    sync
+}
+
+#! call cleanup function on exit (even for ctrl-c interrupt)
+trap cleanup EXIT
+
+
+
+#!
+#! Remount the boot filesystem with read-write permission
+#!
+
+if (( log_file_in_boot )) ; then
+    echo "Mount boot filesystem read-write"
+    sudo -- mount -o remount,rw "${BOOT_DIR}"
+fi
+
+
+
+#!
+#! Execute main program/script
+#!
 
 #cd ${PROG_DIR}
-#pwd
 #echo "Executing ${PROG}"
-sudo ${PROG} "$@" 2>&1 | tee ${LOG_FILE_TMP}
-sudo bash -c "cat ${LOG_FILE_TMP} >> ${LOG_FILE}"
-rm ${LOG_FILE_TMP}
 
-sync
-sudo mount -o remount,ro ${LOG_DIR}
-sync
+#! use this if prog does stdout redirection, not this script !!
+#sudo -- "${PROG}" -o "${LOG_FILE}" "$@"
 
+#! use `script` to allow colours to work with redirection.
+cmd="${PROG} $@ 2>&1"
+sudo -- script -q -c "${cmd}" "${LOG_FILE_TMP}"
+
+#! use `sed` strip colour codes from output and save to file
+sudo -- bash -c "cat ${LOG_FILE_TMP} | sed -r 's/(\x9B|\x1B\[)([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g' >> ${LOG_FILE}"
+
+
+
+#! Exit
+#! cleanup function will be called now (due to trap exit)
