9e41f36055ba07da8a7d2555514967982033eac5
Brendan Simon (SEPL) <brendan@successful.com.au>
2018-10-31 00:55:07 +1100

    [IND][build.sh] Create partition archives for SD Card production
    * Creates `boot`, `root` and `data` partition archive images.

--- ind-efd/build.sh
+++ ind-efd/build.sh
@@ -9,6 +9,9 @@ VERSION="0.12.0-rc2-buster"
 BOARD="ind-efd"
 
 OUT="${BOARD}-v${VERSION}"
+ROOT="${BOARD}-root-v${VERSION}"
+BOOT="${BOARD}-boot-v${VERSION}"
+DATA="${BOARD}-data-v${VERSION}"
 
 OUT_LOG="${OUT}.log"
 
@@ -16,12 +19,21 @@ OUT_TXT="${OUT}.txt"
 
 OUT_TAR="${OUT}.tar"
 
-OUT_TGZ="${OUT_TAR}.gz"
+ROOT_TAR="${ROOT}.tar"
+ROOT_TGZ="${ROOT_TAR}.gz"
+
+BOOT_TAR="${BOOT}.tar"
+BOOT_TGZ="${BOOT_TAR}.gz"
+
+DATA_TAR="${DATA}.tar"
+DATA_TGZ="${DATA_TAR}.gz"
 
 BRICKSTRAP="../brickstrap/brickstrap.sh"
 
 COMMAND=${@:-"all"}
 
+#! could use `mktemp` here
+TMP_DIR="_tmp_dir_"
 
 read -r -d '' USAGE <<-'EOF'
   usage: ./build.sh <command>
@@ -57,20 +69,71 @@ fi
 # -f = force build if output directory already exists.
 #
 
-echo "BOARD = ${BOARD}"
-echo "OUT = ${OUT}"
+echo "BOARD    = ${BOARD}"
+#echo "OUT      = ${OUT}"
+#echo "OUT_TAR  = ${OUT_TAR}"
+#echo "ROOT     = ${ROOT}"
+#echo "ROOT_TAR = ${ROOT_TAR}"
+echo "ROOT_TGZ = ${ROOT_TGZ}"
+#echo "BOOT     = ${BOOT}"
+#echo "BOOT_TAR = ${BOOT_TAR}"
+echo "BOOT_TGZ = ${BOOT_TGZ}"
+#echo "DATA     = ${DATA}"
+#echo "DATA_TAR = ${DATA_TAR}"
+echo "DATA_TGZ = ${DATA_TGZ}"
 
-cmd="${BRICKSTRAP} -b ${BOARD} -d ${OUT} -f ${COMMAND}"
-script -q -c "${cmd}" "${OUT_LOG}"
+#! exit immediately on any errors
+set -e
 
-#! Remove ANSI color codes, etc
-cat "${OUT_LOG}" | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" > "${OUT_TXT}"
+if [ "${COMMAND}" = "all" ] || [  "${COMMAND}" = "create-tar" ] ; then
+    #! run brickstrap script to generate ${OUT_TAR} => ${ROOT_TAR}
+    cmd="${BRICKSTRAP} -b ${BOARD} -d ${OUT} -f ${COMMAND}"
+    script -q -c "${cmd}" "${OUT_LOG}"
+    mv "${OUT_TAR}" "${ROOT_TAR}"
+
+    #! Remove ANSI color codes, etc
+    cat "${OUT_LOG}" | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" > "${OUT_TXT}"
+fi
 
 #
-# Compress the tar image.
+# Make boot filesystem image.
+# Make data filesystem image.
+# Compress the root filesystem image.
+# Make upgrade script.
+# Remove tar image (to save space)
 #
-if [ "${COMMAND}" = "all" ]; then
-    cat ${OUT_TAR} | gzip --best --rsyncable > ${OUT_TGZ} && rm -f ${OUT_TAR}
-    ./makeupgrade.sh && rm -f ${OUT_TGZ}
+if [ "${COMMAND}" = "all" ] || [  "${COMMAND}" = "post" ] ; then
+    #! Make boot filesystem image (extract `/boot/flash/*` from rootfs image)
+    echo "extract and compress boot fs => ${DATA_TGZ}"
+    rm -rf "${TMP_DIR}" && mkdir -p "${TMP_DIR}"
+    tar -x --strip-components=3 -f "${ROOT_TAR}" -C "${TMP_DIR}" "./boot/flash/"
+    tar czf "${BOOT_TGZ}" -C "${TMP_DIR}" .
+    rm -rf "${TMP_DIR}"
+
+    #! Make data filesystem image (extract `/flash/data/*` from rootfs image)
+    echo "extract and compress data fs => ${DATA_TGZ}"
+    rm -rf "${TMP_DIR}" && mkdir -p "${TMP_DIR}"
+    tar -x --strip-components=3 -f "${ROOT_TAR}" -C "${TMP_DIR}" "./flash/data"
+    tar czf "${DATA_TGZ}" -C "${TMP_DIR}" .
+    rm -rf "${TMP_DIR}"
+
+    #! compress rootfs tar image
+    echo "compress root fs => ${ROOT_TGZ}"
+    cat "${ROOT_TAR}" | gzip --best --rsyncable > "${ROOT_TGZ}"
+
+    #! remove root fs tar image
+    rm -f "${ROOT_TAR}"
+
+    #! make upgrade script
+    echo "make upgrade image"
+    ./makeupgrade.sh
+
+    #! Remove data fs files from root fs archive.
+    #! don't remove -- let SD card production tool and upgrade script remove them
+    #gzip -cd "${ROOT_TGZ}" | tar f - --delete "./flash" | gzip -c > "_tmp_${ROOT_TGZ}"
+    #mv "_tmp_${ROOT_TGZ}" "${ROOT_TGZ}"
+
+    #! don't delete root archive -- need for SD card production
+    #rm -f ${ROOT_TGZ}
 fi
 
