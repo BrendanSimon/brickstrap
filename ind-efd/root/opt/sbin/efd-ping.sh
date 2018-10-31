66ad54959151906ddb75978444a9dda2325e3c23
Brendan Simon (SEPL) <brendan@successful.com.au>
2018-10-31 14:27:47 +1100

    [IND][efd-ping] Remove default url if `EFD_PING_SERVERS` not set
    This will effectively disable the EFD Ping.

--- ind-efd/root/opt/sbin/efd-ping.sh
+++ ind-efd/root/opt/sbin/efd-ping.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-##
-## Use curl to do an EFD Ping to the cloud service.
-##
+#!
+#! Use curl to do an EFD Ping to the cloud service.
+#!
+#! EFD_PING_SERVERS must be set in the settings file.
+#! If not set, then no efd-pings will occur.
+#!
 
-## Read user settings file.
+#! Read user settings file.
 source /mnt/data/etc/settings
 
-efd_ping_servers="${EFD_PING_SERVERS:-http://portal.efdweb.com}"
+efd_ping_servers="${EFD_PING_SERVERS}"
 
 efd_ping_api="api/Ping"
 
@@ -15,7 +18,7 @@ serial_number="${SERIAL_NUMBER:-0}"
 
 for server in ${efd_ping_servers} ; do
     url="${server}/${efd_ping_api}/${serial_number}/"
-#    echo "DEUBG: url = ${url}"
+    #echo "DEUBG: url = ${url}"
     curl --silent --request GET ${url} > /dev/null
 done
 
