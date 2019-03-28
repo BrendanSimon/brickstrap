0200387639a7798f7f6fb70ce0faedf40efde5c5
Brendan Simon (SEPL) <brendan@successful.com.au>
2019-03-28 17:29:27 +1100

    [ind-efd][BB#159] Implement periodic reboot feature
    Periodic reboot can be enabled by adding the following in the
    user settings file.
    
    The reboot schedule must comply with `cron` syntax (first 5 fields).
    The first 5 fields are: minute, hour, day-of-month, month, day-of-week
    
    If not present, commented out, or set to "", then the default action
    is no periodic reboots.
    
    PERIODIC_REBOOT_SCHED="0 2 * * *"
    
    Optionaly specify the reboot type: `reboot` or `shutdown`.
    `reboot` does a warm reboot.
    `shutdown` does a `poweroff`, and the system will be rebooted by the PMC
    
    NOTE: The reboot type is ignored for now, until we read the
          battery voltage to ensure the system will indeed reboot.
    
    PERIODIC_REBOOT_TYPE="reboot"

---
+++ ind-efd/root/opt/sbin/periodic_reboot.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#!=============================================================================
+#!
+#! This script performs a system reboot.
+#!
+#! It is intended to be called by anohter process (e.g. a cron job)
+#!
+#!=============================================================================
+
+
+
+#! Read user settings file.
+source /mnt/data/etc/settings
+
+
+
+function do_reboot
+{
+    local cmd="false"
+    local msg="Performing EFD Periodic Reboot"
+
+    #! Ignore `shutdown` type for now, until we check battery voltage is ok !!
+    if (( 0 )) ; then
+#    if [[ "${PERIODIC_REBOOT_TYPE}" == "shutdown" ]] ; then
+        #! shutdown the system will force a power-cycle via the external PMC
+        msg="SHUTDOWN: ${msg}"
+        cmd="/sbin/poweroff"
+    else
+        #! default is warm reboot
+        msg="REBOOT: ${msg}"
+        cmd="/sbin/reboot"
+    fi
+
+    #! send message to stdout
+    echo "${msg}"
+
+    #! send message journald/syslog (tag = name of this script)
+    echo "$msg" | systemd-cat -t "$( basename $0 )"
+
+    #! do the command
+    ${cmd}
+}
+
+
+
+#! call reboot function
+do_reboot
---
+++ ind-efd/root/opt/sbin/periodic_reboot_update.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+#!=============================================================================
+#!
+#! This script checks if the period reboot settings in the user settings file
+#! has changed.
+#!
+#! If it has changed, the script will update the cron file entry,
+#! and restart the service.
+#!
+#!=============================================================================
+
+
+
+#! Read user settings file.
+source /mnt/data/etc/settings
+
+
+
+#!============================================================================
+#! Restart cron service
+#!============================================================================
+function cron_service_restart
+{
+    local ret
+
+    echo "Restarting cron service..."
+
+    systemctl restart cron.service
+
+    ret=$?
+    if (( ret != 0 )); then
+        echo "ERROR: Failed to restart cron service !!"
+    fi
+
+    return ${ret}
+}
+
+
+#!============================================================================
+#! Update Periodic Reboot setting in cron config file.
+#!============================================================================
+function cron_config_update
+{
+    local ret
+
+    if [[ "${periodic_reboot_sched}" == "" ]] ; then
+        #! remove cron config file
+        echo "Removing cron config file: periodic_reboot not set or empty"
+        rm -f "${cron_config}"
+    else
+        #! recreate cron config file
+        echo "Updating cron config file with new Periodic Reboot setting: ${periodic_reboot_sched}"
+        local msg=""
+        msg+="# Perform EFD Periodic Reboot with the following schedule\n"
+        msg+="${new_periodic_reboot_setting}\n"
+        echo -e "${msg}" > "${cron_config}"
+    fi
+
+    ret=$?
+    if (( ret != 0 )); then
+        echo "ERROR: Failed updating cron config file with new Periodic Reboot setting !!"
+    fi
+
+    return ${ret}
+}
+
+
+
+
+#!=============================================================================
+#!
+#! Check if Periodic Reboot setting needs to be updated in cron config file.
+#!
+#!=============================================================================
+
+#! get periodic reboot setting from user settings file.
+periodic_reboot_sched="${PERIODIC_REBOOT_SCHED}"
+
+#! cron config file.
+cron_config="/etc/cron.d/efd_periodic_reboot"
+
+#!
+#! check if cron config file needs updating or removed
+#!
+
+new_periodic_reboot_setting="${periodic_reboot_sched} root /opt/sbin/periodic_reboot.sh"
+
+cur_periodic_reboot_setting=$( fgrep "periodic_reboot.sh" "${cron_config}" 2> /dev/null )
+
+if (( DEBUG )) ; then
+    echo "DEBUG: periodic_reboot_sched = ${periodic_reboot_sched}"
+    echo "DEBUG: new_periodic_reboot_setting = ${new_periodic_reboot_setting}"
+    echo "DEBUG: cur_periodic_reboot_setting = ${cur_periodic_reboot_setting}"
+fi
+
+if [[ "${cur_periodic_reboot_setting}" == "${new_periodic_reboot_setting}" ]] ; then
+    #! cron file does exist
+    echo "EFD Periodic Reboot setting is up to date (enabled)"
+elif [[ "${cur_periodic_reboot_setting}" == "${periodic_reboot_sched}" ]] ; then
+    #! cron file does not exist
+    echo "EFD Periodic Reboot setting is up to date (disabled)"
+else
+    echo "EFD Periodic Reboot setting is out of date (enabled)"
+
+    #! update Periodic Rebboot setting in cron config file.
+    cron_config_update
+    if (( $? != 0 )); then
+        exit $?
+    fi
+
+    #! restart cron service.
+    cron_service_restart
+    if (( $? != 0 )); then
+        exit $?
+    fi
+
+fi
