35c01da723171c4711173cd4b5eb729487976cc7
Brendan Simon (SEPL) <brendan@successful.com.au>
2019-04-02 23:11:03 +1100

    [ind-efd[BB#168] Ensure PMC shutdown request is logged in syslog
    * Changed/added some messages.
    * Ensure messages are flushed.
    * Added comments.

--- ind-efd/root/opt/sbin/sepl_power_supervisor.py
+++ ind-efd/root/opt/sbin/sepl_power_supervisor.py
@@ -1,52 +1,80 @@
 #!/usr/bin/env python2
 
-from time import sleep
+#!============================================================================
+#!
+#!  This script is run at boot and monitors whether a shutdown has been
+#!  requested by the external power management controller and initiates
+#!  the shutdown process.
+#!
+#!  Copyright (C) 2017-2019 Successful Endeavours Pty Ltd
+#!
+#!============================================================================
+
+import sys
+import time
 import subprocess
 
 import ind
 
-##============================================================================
+#!============================================================================
 
 shutdown_cmd = "/sbin/poweroff"
 
 delay = 1
 
-##============================================================================
+#!============================================================================
 
-def led_off(dev_hand=None):
-    ind.battery_led_off(dev_hand=dev_hand)
-    ind.power_led_off(dev_hand=dev_hand)
+def led_off ( dev_hand=None ) :
+    """Turn battery and power LED off"""
 
-##============================================================================
+    ind.battery_led_off( dev_hand=dev_hand )
+    ind.power_led_off( dev_hand=dev_hand )
 
-def led_on(dev_hand=None):
-    ind.battery_led_on(dev_hand=dev_hand)
-    ind.power_led_on(dev_hand=dev_hand)
+#!============================================================================
 
-##============================================================================
+def led_on ( dev_hand=None ) :
+    """Turn battery and power LED on"""
 
-def shutdown():
-    print("Calling command: {}".format(shutdown_cmd))
-    subprocess.call(shutdown_cmd)
-    
-##============================================================================
+    ind.battery_led_on( dev_hand=dev_hand )
+    ind.power_led_on( dev_hand=dev_hand )
 
-def main():
+#!============================================================================
 
-    with open("/dev/IND") as dev_hand:
-        ## Indicate we are up and running.
-        ind.power_os_running_on(dev_hand=dev_hand)
-        led_on(dev_hand=dev_hand)
+def shutdown () :
+    """Shutdown the system"""
+
+    print( "SHUTDOWN: calling '{}'".format( shutdown_cmd ) )
+    sys.stdout.flush()
+
+    subprocess.call( shutdown_cmd )
+
+#!============================================================================
+
+def main () :
+    """Main entry function for the script"""
+
+    with open( "/dev/IND" ) as dev_hand :
+
+        #! indicate to external power management controller that we are up and running.
+        ind.power_os_running_on( dev_hand=dev_hand )
+
+        #! turn leds on
+        led_on( dev_hand=dev_hand )
 
         while True:
-            shutdown_req = ind.power_shutdown_requested(dev_hand=dev_hand)
-            if shutdown_req:
+            #! check if shutdown has been requested (shutdown button pressed)
+            shutdown_req = ind.power_shutdown_requested( dev_hand=dev_hand )
+
+            if shutdown_req :
+                print( "SHUTDOWN_REQUEST detected" )
+                #! kick off the shutdown
                 shutdown()
 
-            sleep(delay)
+            time.sleep( delay )
 
-##============================================================================
+#!============================================================================
 
 if __name__ == "__main__":
     main()
 
+#!============================================================================
--- ind-efd/root/opt/sbin/sepl_shutdown_final.py
+++ ind-efd/root/opt/sbin/sepl_shutdown_final.py
@@ -1,49 +1,79 @@
 #!/usr/bin/env python2
 
-from time import sleep
+#!============================================================================
+#!
+#!  This script is intended to run late in the shutdown process to:
+#!  * flash some LEDs (for visual indication of shutdown).
+#!  * signal the external power management controller that we have shutdown.
+#!  * output some messages to log.
+#!
+#!  Copyright (C) 2017-2019 Successful Endeavours Pty Ltd
+#!
+#!============================================================================
+
+import sys
+import time
 
 import ind
 
-##============================================================================
+#!============================================================================
 
-def led_off(dev_hand=None):
-    #ind.leds_modify(off=0xfffffff, dev_hand=dev_hand)
-    ind.battery_led_off(dev_hand=dev_hand)
-    ind.power_led_off(dev_hand=dev_hand)
-    ind.alert_led_off(dev_hand=dev_hand)
+def led_off ( dev_hand=None ) :
+    """Turn off LEDs."""
 
-##============================================================================
+    #ind.leds_modify( off=0xfffffff, dev_hand=dev_hand )
+    ind.battery_led_off( dev_hand=dev_hand )
+    ind.power_led_off( dev_hand=dev_hand )
+    ind.alert_led_off( dev_hand=dev_hand )
 
-def led_on(dev_hand=None):
-    #ind.leds_modify(on=0xfffffff, dev_hand=dev_hand)
-    ind.battery_led_on(dev_hand=dev_hand)
-    ind.power_led_on(dev_hand=dev_hand)
-    ind.alert_led_on(dev_hand=dev_hand)
+#!============================================================================
 
-##============================================================================
+def led_on ( dev_hand=None ) :
+    """Turn on LEDs."""
 
-def led_cycle(count, delay, dev_hand=None):
-    for i in range(count):
-        led_on(dev_hand=dev_hand)
-        sleep(delay)
-        led_off(dev_hand=dev_hand)
-        sleep(delay)
+    #ind.leds_modify( on=0xfffffff, dev_hand=dev_hand )
+    ind.battery_led_on( dev_hand=dev_hand )
+    ind.power_led_on( dev_hand=dev_hand )
+    ind.alert_led_on( dev_hand=dev_hand )
 
-##============================================================================
+#!============================================================================
+
+def led_cycle ( count, delay, dev_hand=None ) :
+    """Cycle the on/off the LEDs."""
+
+    for i in range( count ):
+        led_on( dev_hand=dev_hand )
+        time.sleep( delay )
+        led_off( dev_hand=dev_hand )
+        time.sleep( delay )
+
+#!============================================================================
 
 def main():
-    with open("/dev/IND") as dev_hand:
-        led_cycle(count=8, delay=0.1, dev_hand=dev_hand)
+    """Main entry function for the script."""
 
-        ind.leds_modify(off=0xfffffff, dev_hand=dev_hand)
-        ind.alert_led_on(dev_hand=dev_hand)
+    print( "SHUTDOWN_FINAL: started" )
 
-        ind.power_os_running_off(dev_hand=dev_hand)
+    with open( "/dev/IND" ) as dev_hand:
 
-        print("Deasserted nOS_RUNNING pin")
+        #! cycle the LEDs for visual indication
+        led_cycle( count=8, delay=0.1, dev_hand=dev_hand )
 
-##============================================================================
+        #! ensure all LEDs are off, except for the alert LED.
+        ind.leds_modify( off=0xfffffff, dev_hand=dev_hand )
+        ind.alert_led_on( dev_hand=dev_hand )
+
+        #! indicate to external power management controller that we have completed shutdown
+        ind.power_os_running_off( dev_hand=dev_hand )
+
+        print( "SHUTDOWN_FINAL: deasserted nOS_RUNNING pin" )
+
+    print( "SHUTDOWN_FINAL: complete" )
+    sys.stdout.flush()
+
+#!============================================================================
 
 if __name__ == "__main__":
     main()
 
+#!============================================================================
